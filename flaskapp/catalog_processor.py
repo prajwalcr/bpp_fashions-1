06ca15fadac0030f172b240038dc08dd99b00f9b
unbxd <unbxd@unbxds-MacBook-Pro.local>
2023-01-20 22:40:07 +0530

    feat($backend): Add API Routes
    -Add API routes for querying product and category info
    -Updat DB Schema into suitable format

--- flaskapp/catalog_processor.py
+++ flaskapp/catalog_processor.py
@@ -1,5 +1,5 @@
 import json
-from flaskapp.models import Product
+from flaskapp.models import Product, Category, Color, Size
 
 from flaskapp import SessionLocal
 
@@ -51,16 +51,59 @@ class JsonCatalogProcessor(CatalogProcessor):
         with SessionLocal() as session:
 
             for dataItem in self.data:
+                id = dataItem["sku"]
+                title = dataItem.get("title", None)
+                if "availability" in dataItem:
+                    availability = dataItem["availability"].lower() == "true"
+                else:
+                    availability = False
+                productDescription = dataItem.get("productDescription", None)
+                imageURL = dataItem.get("productImage", None) # Replace this maybe
+                price = dataItem["price"]
+
+                catlevel1 = dataItem.get("catlevel1Name", None)
+
+                catlevel2 = None
+                if "catlevel2Name" in dataItem:
+                    catlevel2 = dataItem["catlevel2Name"].strip()
+
+                colors = dataItem.get("color", list())
+                sizes = dataItem.get("size", list())
+
                 product = Product(
-                    id=dataItem["sku"],
-                    # availability=dataItem["availability"],
-                    # productDescription=dataItem["productDescription"],
-                    # imageURL=dataItem["productImage"],
-                    price=dataItem["price"]
+                    id=id,
+                    title=title,
+                    availability=availability,
+                    productDescription=productDescription,
+                    imageURL=imageURL,
+                    price=price
                 )
+
+                category = Category(
+                    product_id=id,
+                    catlevel1=catlevel1,
+                    catlevel2=catlevel2
+                )
+
+                colorList = []
+                for color in colors:
+                    colorList.append(Color(product_id=id, product_color=color))
+
+                sizeList = []
+                for size in sizes:
+                    sizeList.append(Size(product_id=id, product_size=size))
+
+                product.category = category
+                product.colors = colorList
+                product.sizes = sizeList
 
                 session.add(product)
                 # time.sleep(0.02)
-            session.commit()
+            try:
+                session.commit()
+            except Exception as error:
+                session.flush()
+                session.rollback()
+                return False
 
         return True
