a2cc653fe4e4b18e85bdb8ca805db5a74ca7b43c
Bhuvan Kumar <bhuvarsenal@gmail.com>
2023-02-03 12:45:48 +0530

    feat:Revising the category dropdown
    Changing the Category dropdown to incorporate sub categories

--- flaskapp/static/js/script.js
+++ flaskapp/static/js/script.js
@@ -1,57 +1,85 @@
 $(document).ready(function(){
-$.ajax({
-  url:'/api/categories',
+jQuery.ajaxSetup({async:false});
+/*$.ajax({
+  url:'/api/products/categories/children/1',
   type:'GET',
   datatype:'JSON',
   success:function(data){
-    console.log("running get categories")
-    $('select[id="category2"]').hide();
-    for(var key in data){
-      var opt='<option value="'+key+'">'+key+'</option>';
-      $(opt).appendTo('#category1')
+    var html ='<div class="dropdown">'
+    html+='<button class="btn btn-info dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" data-mdb-toggle="dropdown" aria-expanded="false">Categories</button>';
+    html+='<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">';
+    for (var i=0;i<data.length;i++){
+      html += '<li><a class="dropdown-item" id="'+data[i]['id']+'"onclick="category('+data[i]['id']+')">'+data[i]['name']+'</a>';
+      $.get('/api/products/categories/children/'+data[i]['id'],function(subdata){
+        console.log(subdata.length)
+        if (subdata.length==0){
+          console.log('hi')
+        }
+        else{
+        html += '<ul class="dropdown-menu dropdown-submenu">'
+        console.log(html)
+        for (var j=0;j<subdata.length;j++){
+          html+='<li><a class="dropdown-item" id="'+subdata[j]['id']+'"onclick="category('+subdata[j]['id']+')">'+subdata[j]['name']+'</a></li>';
+        }
+        html+='</ul>'
+      }
+        })
+      html+='</li>'
+      }
+    html+='</ul>'
+    html+='</div>'
+    $('#facets').append(html)
+  }
+
+})*/
+$.ajax({
+  url:'/api/products/categories/children/1',
+  type:'GET',
+  datatype:'JSON',
+  success:function(data){
+    var html ='<div class="dropdown">'
+    html+='<button class="btn btn-info dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" data-mdb-toggle="dropdown" aria-expanded="false">Categories</button>';
+    html+='<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">';
+    for (var i=0;i<data.length;i++){
+      html += '<li><a class="dropdown-item" id="'+data[i]['id']+'"onclick="category('+data[i]['id']+')">'+data[i]['name']+'</a>';
+      html += subcat(data[i]['id'])
+      html += '</li>'
     }
+    html += '</ul>'
+    $('#facets').append(html)
+  }
+})
     var curl=new URL(window.location.href)
-    cat1=curl.searchParams.get('cat1')
-    cat2=curl.searchParams.get('cat2')
     sort=curl.searchParams.get('sort')
     $('select[id="sort"]').val(sort).change();
-    if (cat1!=null){
-      $('select[id="category1"]').val(cat1).change();
-      $.each(data[cat1],function(key,val){
-        var opt2='<option value="'+val+'">'+val+'</option>';
-        $(opt2).appendTo('#category2')
-        $('select[id="category2"]').show();
-      })}
-      if (cat2!=null){
-        $('select[id="category2"]').val(cat2).change();
-      }
-    $('select[id="category1"]').change(function(){
-      $('#category2 option:not(:first)').remove();
-      var ca1=document.getElementById("category1").value
-      $.each(data[ca1],function(key,val){
-        var opt2='<option value="'+val+'">'+val+'</option>';
-        $(opt2).appendTo('#category2')
-      })
-      $('select[id="category2"]').show();
-    });
-    }
-  })
-  $('button[id="fac"]').click(function(){
-    cat1=document.getElementById('category1').value
-    cat2=document.getElementById('category2').value
-    console.log(cat1,cat2)
-    var url=new URL("http://127.0.0.1:5000/")
-    if (cat1!="" && cat2!=""){
-      url.searchParams.append("cat1",cat1.toString())
-      url.searchParams.append("cat2",cat2.toString())
-    }
-    else if(cat1!="" && cat2==""){
-      url.searchParams.append("cat1",cat1.toString())
-    }
-    window.location.href=url
-    console.log(url)
-  })
-})
 
+  function category(val){
+    ceurl=new URL("http://127.0.0.1:5000/")
+    ceurl.searchParams.append('id',val)
+    console.log(ceurl)
+    window.location.href=ceurl
+  }
+  function subcat(subcatid){
+    var html =''
+    $.get('/api/products/categories/children/'+subcatid,function(subdata){
+      if (subdata.length==0){
+        console.log(html1)
+      }
+      else{
+        html += '<ul class="dropdown-menu dropdown-submenu">'
+        for (var j=0;j<subdata.length;j++){
+          html += '<li><a class="dropdown-item" id="'+subdata[j]['id']+'"onclick="category('+subdata[j]['id']+')">'+subdata[j]['name']+'</a>';
+          html +=subcat(subdata[j]['id'])
+          html += '</li>'
+      }
+      html += '</ul>'
+    
+      }
+      })
+      return html
+
+  }
+
+})
 
   
