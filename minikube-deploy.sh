66433d3480149ca52768dcc8ed40037d0309a26c
prajwalcr <prajwalguptacr@gmail.com>
2023-02-14 19:52:59 +0530

    documentation: Readme update
    Setup information modified in Readme file

--- minikube-deploy.sh
+++ minikube-deploy.sh
@@ -6,7 +6,6 @@ echo "Creating the volume..."
 kubectl apply -f ./kubernetes/persistent-volume.yml
 kubectl apply -f ./kubernetes/persistent-volume-claim.yml
 
-
 echo "Creating credentials..."
 
 kubectl apply -f ./kubernetes/secret.yml
@@ -21,9 +20,9 @@ echo "Creating the postgres deployment and service..."
 
 kubectl create -f ./kubernetes/postgres-deployment.yml
 kubectl create -f ./kubernetes/postgres-service.yml
-#sleep 45
-#POD_NAME=$(kubectl get pod -l service=postgres -o jsonpath="{.items[0].metadata.name}")
-#kubectl exec $POD_NAME --stdin --tty -- createdb -U unbxd bpp
+kubectl wait pod --for=condition=Ready -l service=postgres
+POD_NAME=$(kubectl get pod -l service=postgres -o jsonpath="{.items[0].metadata.name}")
+kubectl exec $POD_NAME --stdin --tty -- createdb -U unbxd bpp
 
 
 echo "Creating the redis deployment and service"
@@ -38,14 +37,16 @@ kubectl create -f ./kubernetes/flaskapp-deployment.yml
 kubectl create -f ./kubernetes/flaskapp-service.yml
 
 
-echo "Adding the ingress..."
-
-kubectl apply -f ./kubernetes/ingress.yml
-
-
 echo "Creating the frontend deployment and service..."
 
 kubectl create -f ./kubernetes/frontend-deployment.yml
 kubectl create -f ./kubernetes/frontend-service.yml
+
+
+echo "Adding the ingress..."
+
+minikube addons enable ingress
+kubectl apply -f ./kubernetes/ingress.yml
+
 
 echo "Done"
