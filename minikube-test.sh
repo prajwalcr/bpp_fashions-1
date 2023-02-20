d2133a234d7691bd236cffaf6e4c09ca5db76a09
prajwalcr <prajwalguptacr@gmail.com>
2023-02-20 10:59:33 +0530

    test: CI/CD workflow
    service name correction

--- minikube-test.sh
+++ minikube-test.sh
@@ -1,2 +1,2 @@
-POD_NAME=$(kubectl get pod -l service=flaskapp -o jsonpath="{.items[0].metadata.name}")
+POD_NAME=$(kubectl get pod -l service=flask -o jsonpath="{.items[0].metadata.name}")
 kubectl exec $POD_NAME --stdin -- pytest
