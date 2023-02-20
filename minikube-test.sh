68344080ef2e0d997b00987db41479d959990874
prajwalcr <prajwalguptacr@gmail.com>
2023-02-20 11:34:03 +0530

    test: CI/CD workflow
    command for extracting api pod modified

--- minikube-test.sh
+++ minikube-test.sh
@@ -1,5 +1,3 @@
 kubectl get service
-kubectl get pod -l service=postgres -o jsonpath="{.items[0].metadata.name}"
-kubectl get pod -l service=flask -o jsonpath="{.items[0].metadata.name}"
-POD_NAME=$(kubectl get pod -l service=flask -o jsonpath="{.items[0].metadata.name}")
+POD_NAME=$(kubectl get pod -l app=flask -o jsonpath="{.items[0].metadata.name}")
 kubectl exec $POD_NAME --stdin -- pytest
