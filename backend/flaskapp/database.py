28e620c896487336d8c88b6b4a4a6de8db3ace72
Prajwal Santosh <prajwalsantosh@gmail.com>
2023-02-20 12:26:25 +0530

    documentation: Readthedocs
    Publishing code documentation

--- backend/flaskapp/database.py
+++ backend/flaskapp/database.py
@@ -10,7 +10,8 @@ load_dotenv()
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 # SQLALCHEMY_DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@postgres:5432/bpp'
-SQLALCHEMY_DATABASE_URI = f'postgresql://postgres:postgres@localhost:5432/postgres'
+# SQLALCHEMY_DATABASE_URI = f'postgresql://postgres:postgres@localhost:5432/postgres'
+SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
