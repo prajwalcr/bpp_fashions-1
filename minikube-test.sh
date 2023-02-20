f8f0ad62f4ca0376418446eb1becb0db49f39757
prajwalcr <prajwalguptacr@gmail.com>
2023-02-20 11:16:25 +0530

    test: CI/CD workflow
    debugging service not found bug

--- minikube-test.sh
+++ minikube-test.sh
@@ -1,3 +1,5 @@
 kubectl get service
+kubectl get pod -l service=postgres -o jsonpath="{.items[0].metadata.name}"
+kubectl get pod -l service=flask -o jsonpath="{.items[0].metadata.name}"
 POD_NAME=$(kubectl get pod -l service=flask -o jsonpath="{.items[0].metadata.name}")
 kubectl exec $POD_NAME --stdin -- pytest
