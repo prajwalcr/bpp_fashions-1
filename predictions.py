d9452ad4a9a62312a902e06e00fddc471b964fa3
Bhuvan Kumar <bhuvarsenal@gmail.com>
2023-02-21 12:18:47 +0530

    fix:Changing the predictions outcome

--- predictions.py
+++ predictions.py
@@ -3,23 +3,27 @@ import json
 with open('products_prob.json') as f:
    products_prob = json.load(f)
 
-index=products_prob['columns'].index('00103086')
-
-ratings=products_prob['data']
-indexratings=[]
-for i in ratings:
-    indindexratings=[]
-    for j in range(len(i)):
-        indindexratings.append([i[j],j])
-    indexratings.append(indindexratings)
-s=indexratings[index]
-sorte=sorted(s,key=lambda x:x[0],reverse=True)
-no_of_suggestions=5
-pred=sorte[:no_of_suggestions]
-finalpred=[]
-for i in pred:
-    finalpred.append(products_prob['columns'][i[1]])
-print(finalpred)
+#Specify product id here to get recommendations here
+prodid='abcd'
+if prodid not in products_prob['columns']:
+    print('[]')
+else:
+    index=products_prob['columns'].index(prodid)
+    ratings=products_prob['data']
+    indexratings=[]
+    for i in ratings:
+        indindexratings=[]
+        for j in range(len(i)):
+            indindexratings.append([i[j],j])
+        indexratings.append(indindexratings)
+    s=indexratings[index]
+    sorte=sorted(s,key=lambda x:x[0],reverse=True)
+    no_of_suggestions=5
+    pred=sorte[:no_of_suggestions]
+    finalpred=[]
+    for i in pred:
+        finalpred.append(products_prob['columns'][i[1]])
+    print(finalpred)
 
 '''
 basket = ['The contrast between the plastic frames and metal brow bar gives these sunglasses a sleek look you can wear with confidence.']
